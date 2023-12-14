1b494689ad718464da06e0f3bd9e879a4147460a
Valentin Thibault <valentin.thlt@gmail.com>
2023-12-14 21:31:30 +0100

    feat: create attributs and abstract function Creature

--- src/main/java/fr/zoraax/pokemon/classes/Creature.java
+++ src/main/java/fr/zoraax/pokemon/classes/Creature.java
@@ -1,4 +1,13 @@
 package fr.zoraax.pokemon.classes;
 
 public abstract class Creature {
+    protected String nom;
+    protected double pointDeVie;
+    protected double pointDeVieMax;
+    protected Type[] type;
+
+    abstract void attaquer(Creature creature, Attaque attaque);
+    abstract void recevoirDegats(int degats);
+    abstract void coupCritique();
+    abstract void esquive();
 }
