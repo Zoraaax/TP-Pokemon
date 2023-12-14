33fc7518a346a8520ee4e634ace5dc90edb4396f
Valentin Thibault <valentin.thlt@gmail.com>
2023-12-14 22:06:41 +0100

    feat: create methods to faiblesse and resistance

--- src/main/java/fr/zoraax/pokemon/classes/Type.java
+++ src/main/java/fr/zoraax/pokemon/classes/Type.java
@@ -11,6 +11,40 @@ public class Type {
         this.resistance = resistance;
     }
 
+    /**
+     * Ajoute une faiblesse au type choisit
+     * @param type - Type à ajouter
+     */
+    public void ajouterFaiblesse(Type type) {
+        Type[] faiblesse = this.getFaiblesse();
+        faiblesse[faiblesse.length] = type;
+        this.setFaiblesse(faiblesse);
+    }
+
+    /**
+     * Ajoute une résistance au type choisit
+     * @param type - Type à ajouter
+     */
+    public void ajouterResistance(Type type) {
+        Type[] resistance = this.getResistance();
+        resistance[resistance.length] = type;
+        this.setResistance(resistance);
+    }
+
+    /**
+     * Vérifie si le type est fort contre le type choisit
+     * @param type - Type à vérifier
+     * @return Booléen - Retourne true si le type est fort contre le type choisit
+     */
+    public boolean estFaibleContre(Type type) {
+        for (Type faiblesse : this.getFaiblesse()) {
+            if (faiblesse == type) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public String getNom() {
         return this.nom;
     }
